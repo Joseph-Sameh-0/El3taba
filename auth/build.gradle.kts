7f47015f332369d19aa89687ff636b18b27bfe09
Joseph-Sameh-0 <148119993+Joseph-Sameh-0@users.noreply.github.com>
2024-09-11 00:19:43 +0300

    viewBinding = true

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
 }
 
 android {
+    buildFeatures {
+        viewBinding = true
+    }
+
     namespace = "com.example.auth"
     compileSdk = 34
 
@@ -37,6 +41,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- customer/build.gradle.kts
+++ customer/build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
 }
 
 android {
+    buildFeatures {
+        viewBinding = true
+    }
+
     namespace = "com.example.customer"
     compileSdk = 34
 

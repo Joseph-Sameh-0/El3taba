037dba1078c5532645b92696adce7ab4fbb540a0
Joseph-Sameh-0 <148119993+Joseph-Sameh-0@users.noreply.github.com>
2024-09-15 21:21:05 +0300

    add initial customer fragments and navigation and menu

---
+++ app/src/main/java/com/example/el3taba/customer/bag/BagFragment.kt
@@ -0,0 +1,41 @@
+package com.example.el3taba.customer.ui.bag
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.el3taba.databinding.CustomerFragmentBagBinding
+
+class BagFragment : Fragment() {
+
+private var _binding: CustomerFragmentBagBinding? = null
+  // This property is only valid between onCreateView and
+  // onDestroyView.
+  private val binding get() = _binding!!
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    val bagViewModel =
+            ViewModelProvider(this).get(BagViewModel::class.java)
+
+    _binding = CustomerFragmentBagBinding.inflate(inflater, container, false)
+    val root: View = binding.root
+
+    val textView: TextView = binding.textBag
+    bagViewModel.text.observe(viewLifecycleOwner) {
+      textView.text = it
+    }
+    return root
+  }
+
+override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/el3taba/customer/bag/BagViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.el3taba.customer.ui.bag
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class BagViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is bag Fragment"
+    }
+    val text: LiveData<String> = _text
+}

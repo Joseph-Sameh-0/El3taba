4ace17bc3f4f12d10ed0180218f8de796a9d89d3
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2024-10-02 00:29:54 +0300

    .

--- app/src/main/java/com/example/el3taba/auth/ForgetPasswordFragment.kt
+++ app/src/main/java/com/example/el3taba/auth/ForgetPasswordFragment.kt
@@ -6,14 +6,33 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.el3taba.R
+import com.example.el3taba.databinding.AuthFragmentForgetPasswordBinding
 
 
 class ForgetPasswordFragment : Fragment() {
+    private var _binding: AuthFragmentForgetPasswordBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.auth_fragment_forget_password, container, false)
+        _binding = AuthFragmentForgetPasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Handle Sign-up button click
+        binding.SubmitButton.setOnClickListener {
+            // Here you can retrieve the input values and handle sign-up logic
+            val email = binding.emailEditText.text.toString()
+            val oldPassword = binding.oldPasswordEditText.text.toString()
+            val newPassword = binding.newPasswordEditText.text.toString()
+            val confirmPassword = binding.confirmPasswordEditText.text.toString()
+
+            // Perform validation and other logic here
+        }
     }
 }

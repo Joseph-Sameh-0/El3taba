6ab70f52321dd7737d23fa77cb8ba407315b0a99
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2024-10-12 00:58:30 +0300

    .

--- app/src/main/java/com/example/el3taba/customer/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/el3taba/customer/profile/ProfileFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity.MODE_PRIVATE
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import com.example.el3taba.R
 import com.example.el3taba.databinding.CustomerFragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -28,17 +30,12 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.logoutButton.setOnClickListener {
-            val sharedPreferences =
-                requireActivity().getSharedPreferences("user_session", MODE_PRIVATE)
-            sharedPreferences.edit().clear().apply()
-
-            val intent = requireActivity().intent
-            requireActivity().finish()
-            startActivity(intent)
-        }
-
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.nav_host_profile) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navGraph = navController.navInflater.inflate(R.navigation.customer_profile_nav)
+        navController.graph = navGraph
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()

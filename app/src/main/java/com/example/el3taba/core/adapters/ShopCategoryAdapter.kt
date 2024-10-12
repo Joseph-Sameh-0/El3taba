a067266d352e023577bb1e3c28c1c01b9369d448
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2024-10-12 11:24:22 +0300

    .

--- app/src/main/java/com/example/el3taba/core/adapters/ShopCategoryAdapter.kt
+++ app/src/main/java/com/example/el3taba/core/adapters/ShopCategoryAdapter.kt
@@ -3,8 +3,9 @@ package com.example.el3taba.core.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.el3taba.core.dataClasses.Category
+import com.bumptech.glide.Glide
 import com.example.el3taba.databinding.ItemCategoryBinding
+import com.example.el3taba.seller.myProducts.Category
 
 class ShopCategoryAdapter(
     private val categories: List<Category>,
@@ -15,7 +16,6 @@ class ShopCategoryAdapter(
         RecyclerView.ViewHolder(binding.root) {
         fun bind(category: Category) {
             binding.categoryTitle.text = category.name
-            binding.categoryImage.setImageResource(category.imageResId)
             binding.root.setOnClickListener {
                 onItemClick(category)
             }
@@ -30,6 +30,10 @@ class ShopCategoryAdapter(
 
     override fun onBindViewHolder(holder: ShopCategoryViewHolder, position: Int) {
         holder.bind(categories[position])
+
+        Glide.with(holder.itemView.context)
+            .load(categories[position].imageUrl)
+            .into(holder.binding.categoryImage)
     }
 
     override fun getItemCount(): Int = categories.size
